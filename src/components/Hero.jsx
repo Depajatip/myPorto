57efd8a872e0b7ea4a6f1a9f45053e32870e309d
depajatip <defajati0@gmail.com>
2025-12-06 21:48:13 +0700

    fix folder direction

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -105,7 +105,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Defaa</span>
+            Hi, I'm <span className='text-[#915EFF]'>Defaaa</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             software enginner student. <br className='sm:block hidden'/> 
